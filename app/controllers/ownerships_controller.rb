b1b896688c33eadc110f4074748c57cc1114468b
ooohiroyukiooo <hiroyuki.takayama1985@gmail.com>
2015-08-14 11:33:27 +0000

    add item_lookup

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -10,8 +10,14 @@ class OwnershipsController < ApplicationController
 
     # itemsテーブルに存在しない場合はAmazonのデータを登録する。
     if @item.new_record?
+      begin
       # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-      response = {}
+        #response = Amazon::Ecs.item_lookup(@item)
+        response =  Amazon::Ecs.item_lookup(params[:asin] , response_group: 'Medium' , country: 'jp')
+      rescue Amazon::RequestError => e
+        return render :js => "alert('#{e.message}')"
+      end
+      
       amazon_item       = response.items.first
       @item.title        = amazon_item.get('ItemAttributes/Title')
       @item.small_image  = amazon_item.get("SmallImage/URL")
@@ -23,12 +29,28 @@ class OwnershipsController < ApplicationController
     end
 
     # ユーザにwant or haveを設定する
+    if params[:type] == "Want"
+      current_user.want(@item_id)
+    end
+    
+    if params[:type] == "Have"
+      current_user.have(@item_id)
+    end
+    
   end
 
   def destroy
     @item = Item.find(params[:item_id])
 
     # TODO 紐付けの解除
-
+    if params[:type] == "Want"
+      @item = .
+      current_user.unwant(@item)
+    end
+    
+    if params[:type] == "Have"
+      @item = .
+      current_user.unhave(@item)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,14 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :relationships, only: [:create, :destroy]
   resources :ownerships, only: [:create, :destroy]
-  resources :items , only: [:new , :show]
+  resources :items, only: [:new , :show]
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
