bd8bb9fc96f4ce303bf4b7b7204f0af2f097c666
ooohiroyukiooo <hiroyuki.takayama1985@gmail.com>
2015-08-18 11:14:20 +0000

    add meta title and description

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  get 'ranking/have' # Haveのランキングページ
+  get 'ranking/want' # Wantのランキングページ
   
 
   resources :users
@@ -13,7 +15,6 @@ Rails.application.routes.draw do
   resources :relationships, only: [:create, :destroy]
   resources :ownerships, only: [:create, :destroy]
   resources :items, only: [:new , :show]
-  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
