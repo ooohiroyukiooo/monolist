1d599f8d88f0fbf14823726296a20545629e6fb1
ooohiroyukiooo <hiroyuki.takayama1985@gmail.com>
2015-08-19 07:29:39 +0000

    fix RankingController

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,12 +2,14 @@ class RankingController < ApplicationController
 
     def have
       haveids = Have.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
-      @items = Item.find(haveids)
+      #@items = Item.find(haveids) 配列haveidsの中の順序が維持されない
+      @items = haveids.map {|id| Item.find(id)}
     end
     
     def want
       wantids = Want.group(:item_id).order('count_item_id desc').limit(10).count(:item_id).keys
-      @items = Item.find(wantids)
+      #@items = Item.find(wantids)
+      @items = wantids.map {|id| Item.find(id)}
     end
 
 end
